62831be9f80a6849671b50b5d1c9cc657f6c3698
jtdv01 <jtdv01@gmail.com>
2019-07-30 23:06:09 +1000

    Some model out

---
+++ assignment-fnc/config.py
@@ -0,0 +1,8 @@
+
+
+labels = {
+    'agree': 0,
+    'disagree': 1,
+    'discuss': 2,
+    'unrelated': 3
+}
--- assignment-fnc/main.py
+++ assignment-fnc/main.py
@@ -2,8 +2,17 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import feature_extraction
 import scipy.sparse as sp
+import re
+import nltk
+import numpy as np
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+
+
 import pdb
 
+# Sourced from: https://github.com/FakeNewsChallenge/fnc-1-baseline
+_wnl = nltk.WordNetLemmatizer()
 
 def normalize_word(w):
     """
@@ -41,13 +50,14 @@ def refuting_features(headlines, bodies):
         'retract'
     ]
     X = []
-    for i, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+    for i, (headline, body) in enumerate(zip(headlines, bodies)):
         clean_headline = clean(headline)
         clean_headline = get_tokenized_lemmas(clean_headline)
         features = [1 if word in clean_headline else 0 for word in _refuting_words]
         # Return a count of refuting words
         features = sum(features)
         X.append(features)
+    X = np.array([X]).T
     return X
 
 def remove_stopwords(l):
@@ -101,22 +111,45 @@ def tfvectorizer(train, test):
     tfidf.fit(corpus)
     return tfidf
 
+labels = {
+    'agree': 0,
+    'disagree': 1,
+    'discuss': 2,
+    'unrelated': 3
+}
 
 if __name__ == '__main__':
+    print("Reading data..")
     train_bodies, train_stances, test_bodies, test_stances = read_comp_data()
     train = merge_stance_and_body(train_stances, train_bodies)
     test = merge_stance_and_body(test_stances, test_bodies)
 
-    pdb.set_trace()
     # Count refutes
+    print("Counting the number of refutes...")
     train_refutes = count_refutes(train)
     test_refutes = count_refutes(test)
 
     # Transform with tfidf
+    print("Fitting a tfidf vectorizer..")
     tfidf = tfvectorizer(train, test)
 
     train_words = tfidf.transform(train['articleBody'])
 
-    # hstack the 
+    # Combine refutes and words
+    train_X = sp.hstack((train_refutes, train_words))
 
+
+    pdb.set_trace()
+    # Train a model on this...
+    train_Y = train['Stance'].apply(lambda key: labels[key])
+
+    model = DecisionTreeClassifier()
+    model.fit(train_X, train_Y)
+    train_score = model.score(train_X, train_Y)
+
+
+
+    pickle.dump(train_refutes_words, open( "pickles/train.pickle", "wb" ) )
+
+    print("Done")
 
