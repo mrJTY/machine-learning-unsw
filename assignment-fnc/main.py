d6a29d2291126e54af72088a3e24490800db22a5
jtdv01 <jtdv01@gmail.com>
2019-08-04 19:32:01 +1000

    Modelling with fnc

--- assignment-fnc/main.py
+++ assignment-fnc/main.py
@@ -6,7 +6,7 @@ import fnc_challenge_utils.scoring as scoring
 
 def parse_args():
     parser = argparse.ArgumentParser();
-    parser.add_argument("--process_data", help="If flag is true, then it will process and vectorize words from the train and test sets. Otherwise, it will load .pickle files", action="store_true")
+    parser.add_argument("--skip_preprocess", help="If flag is true, it will skip preprocessing the data and load .pickle files in the data/ folder", action="store_true")
     parser.add_argument("--model", help="Model name", required=True)
     parser.add_argument("--train_prop", help="The proportion of training dataset to read from. Between 0 and 1", default=1)
     parser.add_argument("--test_prop", help="The proportion of training dataset to read from. Between 0 and 1", default=1)
@@ -21,28 +21,16 @@ if __name__ == '__main__':
     train_prop = float(args.train_prop)
     test_prop = float(args.test_prop)
 
-    if args.process_data:
+    if args.skip_preprocess:
+        pass
+    else:
          pe.preprocess_data(datasources=config.DATASOURCES, train_prop=train_prop, test_prop=test_prop)
 
     train_X, train_Y, test_X, test_Y = pe.load_pickles()
 
-    # TODO(JT): Add kfold
-    # for fold in fold_stances:
     clf = mo.train_model(args.model, train_X, train_Y, test_X, test_Y)
 
     predicted = [config.LABELS[int(a)] for a in clf.predict(test_X)]
     actual = [config.LABELS[int(a)] for a in test_Y]
     score = scoring.report_score(actual, predicted)
-
-    # TODO(JT): Find the best score
-    #         fold_score, _ = scoring.score_submission(actual, predicted)
-    #         max_fold_score, _ = scoring.score_submission(actual, actual)
-    # 
-    #         score = fold_score/max_fold_score
-    # 
-    #         print("Score for fold "+ str(fold) + " was - " + str(score))
-    #         if score > best_score:
-    #             best_score = score
-    #             best_fold = clf
-
 
--- assignment-fnc/sbin/tree-scores.sh
+++ assignment-fnc/sbin/tree-scores.sh
@@ -1,6 +1,5 @@
 conda activate fnc
 
-# Tree model
 python main.py --model tree --process_data --train-prop 0.25 > logs/tree_0.25.log
 python main.py --model tree --process_data --train-prop 0.50 > logs/tree_0.50.log
 python main.py --model tree --process_data --train-prop 0.75 > logs/tree_0.75.log
