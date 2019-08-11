1154bf9044cfd7fe4fa1e055e611ff0a3baad5af
jtdv01 <jtdv01@gmail.com>
2019-08-11 13:20:15 +1000

    Cleaned up data

--- assignment-fnc/sbin/1-download-datasets.sh
+++ assignment-fnc/sbin/1-download-datasets.sh
@@ -1,2 +1,3 @@
 mkdir -p data/
-cd data/ && git clone https://github.com/FakeNewsChallenge/fnc-1/tree/29d473af2d15278f0464d5e41e4cbe7eb58231f2
+git clone https://github.com/FakeNewsChallenge/fnc-1.git
+mv fnc-1/* data/
